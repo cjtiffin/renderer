aa7e9b210ed781fe167c1f187fac1bd316caf5ce
Chris Tiffin <cjtiffin@gmail.com>
2016-03-29 20:27:19 +0100

    Adding double vector typedef

--- vector.h
+++ vector.h
@@ -97,10 +97,15 @@ public:
 };
 
 typedef  Vector2<float>  Vector2f;
+typedef  Vector2<double> Vector2d;
 typedef  Vector2<int>    Vector2i;
+
 typedef  Vector3<float>  Vector3f;
+typedef  Vector3<double> Vector3d;
 typedef  Vector3<int>    Vector3i;
+
 typedef  Vector4<float>  Vector4f;
+typedef  Vector4<double> Vector4d;
 typedef  Vector4<int>    Vector4i;
 
 #endif //_VECTOR_H_
